d83d4821869234ebcab96792e91b86f878419c4f
Zhao Shu <zhaobenben007@googlemail.com>
2019-03-18 21:32:46 +0800

    remove data.sql

--- business/message-service/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO product (id, name, description) VALUES (1, 'book', 'MySQL in Action');
-INSERT INTO product (id, name, description) VALUES (2, 'clothes', 'Zara');
-INSERT INTO product (id, name, description) VALUES (3, 'book', 'Historian');
